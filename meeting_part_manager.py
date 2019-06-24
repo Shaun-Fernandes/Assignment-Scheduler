4d82759ec8139772ff056932eb44e6f342fe87e7
Shaun <shaunferns99@gmail.com>
2019-06-24 22:20:30 +0400

    Get number of parts from the date position
    Edit the meeting_part_manager.py file. Change the code to find the number of parts automatically using the input date field. Previously it looked for the string 'Enter the start date below' but this is more robust to change.

--- meeting_part_manager.py
+++ meeting_part_manager.py
@@ -12,69 +12,55 @@ class MeetingPartManager:
     """
 
     def __init__(self, in_file_name:str = "input_file.xlsx", out_file_name:str
-                = "template.xlsx", date_cell:str = "J2") -> 'MeetingPartManager':
+                = "template.xlsx") -> 'MeetingPartManager':
         """Initialize the Meeting Part Manager class with input file name, output
         file name, and the cell that contains the start date in the input file"""
 
-        input_wb = load_workbook(in_file_name, read_only=True)
+        input_wb = load_workbook(in_file_name)
         self.input_sheet = input_wb.worksheets[0]
-        self.output_wb = load_workbook(out_file_name, read_only=False)
+        self.output_wb = load_workbook(out_file_name)
         self.output_sheet = self.output_wb.worksheets[0]
         self.output_sheet2 = self.output_wb.worksheets[1]
-        self.date_cell = date_cell
+        # self.date_cell = date_cell
         self.meeting_parts = []
         self.list_of_names = []
 
         self.get_start_date()
-        self.create_Parts()
+        self.create_parts()
 
 
     def get_start_date(self):
         """Read the start date from the input file. Default to todays date otherwise."""
 
-        # self.found_date = False
-        # self.no_of_parts = 9
-        # second_row = self.input_sheet[2]
-        # second_row = [second_row[i].value for i in range(len(second_row))]      # convert row cell to row list
-        # for i in range(len(second_row)):
-        #     x = second_row[i]
-        #     if type(second_row[i]) == datetime.datetime:
-        #         self.start_date = second_row[i]
-        #         self.no_of_parts = i - 1
-        #         self.found_date = True
-        #         break
+        self.no_of_parts = 9
+        self.start_date = datetime.date.today()
+        found_date = False
+        second_row = self.input_sheet[2]
+        second_row = [second_row[i].value for i in range(len(second_row))]      # Convert row cell to list
+        for i in range(len(second_row)):
+            if type(second_row[i]) == datetime.datetime:
+                self.start_date = second_row[i]
+                self.no_of_parts = i - 1            # Do not include the labels column 'A' (-1)
+                self.found_date = True
+                break
 
-
-
-        entered_date = self.input_sheet[self.date_cell].value    # Local Variable
-        if (entered_date is not None) and not (type(entered_date) == datetime.datetime):
-            print("Unable to read the entered date.")
-            print("In the input file, please change the format of the cell for the start date to type 'date' (short date or long date will do)\n")
-
-        if (entered_date is not None) and (type(entered_date) == datetime.datetime):
-            start_date = entered_date                       # Local variable
-        else:
+        if not found_date:
             print("Defaulting to todays date as input for the start date...")
-            start_date = datetime.date.today()
-        self.start_date = start_date
 
 
     def create_parts(self):
         """Create objects of class MeetingPart and store them in the meeting_parts list.
         Also add all the names to the names list"""
 
-        first_row = self.input_sheet[1]
-        try:
-            first_row = [first_row[i].value for i in range(len(first_row))]         # convert row cell to row list
-            self.no_of_parts = first_row.index("Enter the start date below") - 1    # Do not include the labels column 'A' (-1)
-        except:
-            self.no_of_parts = 9
-
-        for i in range(self.no_of_parts):
-            col = chr(ord('B') + i)
+        for i in range(1, self.no_of_parts):
+            col = chr(ord('A') + i)
             mp = MeetingPart(self.input_sheet, col)
             self.meeting_parts.append(mp)
             self.list_of_names.append(mp.get_names())
+
+
+    def randomize_list(self):
+        pass
 
 
     def save_to_file(self):
@@ -131,6 +117,8 @@ class MeetingPartManager:
 
 
 if __name__ == '__main__':
-    MPM = MeetingPartManager("input_file.xlsx", "template.xlsx", "J2")
+    MPM = MeetingPartManager("input_file.xlsx", "template.xlsx")
+    MPM.no_of_parts
     MPM.start_date
-    MPM.save_to_file()
+    MPM.list_of_names
+    # MPM.save_to_file()
